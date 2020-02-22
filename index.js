15e7f81e065ba0b16da18fed71e4cde0c07d4e2b
Berat Bozkurt <beratbozkurt1999@gmail.com>
2020-02-22 15:57:09 +0300

    sunucu eklendi ve çalıştı

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const db = require('./config')
+
+
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+
+app.get('/users', db.getUsers)
+
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+})
