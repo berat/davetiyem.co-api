97f2fd8ffe04797dd49078636707ddc3205e98d8
Berat Bozkurt <beratbozkurt1999@gmail.com>
2020-03-26 13:10:28 +0300

    bugs fixed

--- querys/infPersonal.js
+++ querys/infPersonal.js
@@ -7,6 +7,7 @@ const fs = require('fs')
 
 const kisiselBilgiler = (request, response) => {
   const { gelinAdi, gelinBio, damatAdi, damatBio, userid } = request.body
+
   pool.query(
     'SELECT * FROM bilgi WHERE userid = $1',
     [userid],
@@ -16,61 +17,33 @@ const kisiselBilgiler = (request, response) => {
       }
       if (results.rowCount == 1) {
         pool.query(
-          'SELECT * from users Where userid = $1',
-          [userid],
+          'UPDATE "bilgi" SET "gelinAdi" = $1, "damatAdi" = $2, "gelinBio" = $3, "damatBio" = $4 WHERE "userid" = $5',
+          [gelinAdi, damatAdi, gelinBio, damatBio, userid],
           (error, results) => {
             if (error) {
               throw error
-            } else {
-              pool.query(
-                'UPDATE "bilgi" SET "gelinAdi" = $1, "damatAdi" = $2, "gelinBio" = $3, "damatBio" = $4 WHERE "userid" = $5',
-                [gelinAdi, damatAdi, gelinBio, damatBio, userid],
-                (error, result) => {
-                  if (error) throw error
-                  response.send({
-                    status: 201,
-                    msg: 'kişisel bilgileriniz güncellendi'
-                  })
-                }
-              )
             }
-            response.send({
-              status: 404,
-              msg: 'Bilgiler güncellenemedi'
-            })
           }
         )
+        response.send({
+          status: 201,
+          msg: 'kişisel bilgileriniz güncellendi'
+        })
       } else if (results.rowCount == 0) {
         pool.query(
-          'SELECT * from users WHERE userid = $1',
-          [userid],
+          'INSERT INTO bilgi ("userid", "gelinAdi", "damatAdi", "gelinBio", "damatBio") VALUES ($1, $2, $3, $4, $5)',
+          [userid, gelinAdi, damatAdi, gelinBio, damatBio],
           (error, results) => {
             if (error) {
               throw error
-            } else {
-              pool.query(
-                'INSERT INTO bilgi (userid, gelinAdi, damatAdi, gelinBio, damatBio) VALUES ($1, $2, $3, $4)',
-                [userid, gelinAdi, damatAdi, gelinBio, damatBio],
-                (error, result) => {
-                  if (error) throw error
-                  response.send({
-                    status: 201,
-                    msg: 'kişisel bilgileriniz oluştu'
-                  })
-                }
-              )
             }
-            response.send({
-              status: 404,
-              msg: 'Bilgiler kayıt edilemedi'
-            })
           }
         )
+        response.send({
+          status: 201,
+          msg: 'kişisel bilgileriniz oluştu'
+        })
       }
-      response.send({
-        status: 404,
-        msg: 'Site admini ile iletişime geçin.'
-      })
     }
   )
 }
@@ -221,7 +194,6 @@ const gelinFotoYukle = (request, response) => {
 
 const damatFotoYukle = (request, response) => {
   const userid = request.params.hash
-
   pool.query(
     'SELECT * FROM "bilgi" WHERE "userid" = $1',
     [userid],
@@ -279,7 +251,7 @@ const damatFotoYukle = (request, response) => {
                         response.send({
                           status: 201,
                           msg: 'Damat Fotoğrafı Yüklendi',
-                          data: response
+                          data: request.files.damatFoto[0]
                         })
                       }
                     }
