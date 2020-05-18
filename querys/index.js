fea0c67b8abdf455c0ea639d7732584f6b2365d1
Berat Bozkurt <beratbozkurt1999@gmail.com>
2020-05-18 14:56:01 +0300

    cors extra eklendi

--- querys/index.js
+++ querys/index.js
@@ -26,6 +26,8 @@ var allowedOrigins = ['http://localhost:3000', 'https://davetiyem.co']
 app.use(
   cors({
     origin: function(origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
       if (!origin) return callback(null, true)
       if (allowedOrigins.indexOf(origin) === -1) {
         var msg =
@@ -34,7 +36,9 @@ app.use(
         return callback(new Error(msg), false)
       }
       return callback(null, true)
-    }
+    },
+
+    credentials: true
   })
 )
 
