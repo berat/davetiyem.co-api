72b5f5a1528807714e9714103fb1f05a9422ddb0
Berat Bozkurt <beratbozkurt1999@gmail.com>
2020-08-13 01:11:07 +0300

    bugs fixed

--- querys/index.js
+++ querys/index.js
@@ -57,7 +57,7 @@ app.use(function(req, res, next) {
 })
 
 app.all('/*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Origin', 'https://davetiyem.co/')
   res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
   next()
 })
