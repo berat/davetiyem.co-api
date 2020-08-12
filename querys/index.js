f65b98bf7946cf5107612468ff5c49495319d307
Berat Bozkurt <beratbozkurt1999@gmail.com>
2020-08-13 01:09:39 +0300

    bugs fixed

--- querys/index.js
+++ querys/index.js
@@ -48,10 +48,7 @@ app.use(function(req, res, next) {
   )
 
   // izin vermek istedigin header
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
 
   // eger siten cookie alsin istiosan request'te
   res.setHeader('Access-Control-Allow-Credentials', true)
@@ -61,7 +58,7 @@ app.use(function(req, res, next) {
 
 app.all('/*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
   next()
 })
 
