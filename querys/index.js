f93f48e7d211734dcbbf9194fde213607707b31a
Berat Bozkurt <beratbozkurt1999@gmail.com>
2020-08-13 18:08:18 +0300

    bugs fixed

--- querys/index.js
+++ querys/index.js
@@ -22,17 +22,17 @@ app.use(
     extended: true
   })
 )
-
 app.use(cors())
 
 var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://davetiyem.co/')
+  res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
   )
 
+  // intercept OPTIONS method
   if ('OPTIONS' == req.method) {
     res.send(200)
   } else {
